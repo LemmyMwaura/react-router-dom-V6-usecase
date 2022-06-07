6018c1d4c10ee272302ba6980515d17114e70967
Lemmy Mwaura <lemminton@gmail.com>
2022-06-07 22:44:24 +0300

    refactor: add routes to app component and refactor the index.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './components/Auth';
+import Admin from './components/Admin';
+import React,{ lazy, Suspense} from 'react';
+import HomeComponent from './components/Home';
+import NavbarComponent  from './components/Navbar';
+import NoMatchComponent from './components/NoMatch';
+import OrderSummary from './components/OrderSummary';
+import UserComponent from './components/Users';
+import ProductsComponent from './components/Products';
+import UserDetails from './components/Userdetails';
+import Profile from './components/Profile';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { Newproducts } from './components/Newproducts';
+import Login from './components/Login';
+import RequireAuth from './components/RequireAuth';
+
+// eslint-disable-next-line
+const lazyAbout = lazy(() => import('./components/About'));
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <AuthProvider>
+      <NavbarComponent/>
+      <Routes>
+        <Route path='/' element={<HomeComponent/>}/>
+        <Route path='/about'
+          element={
+            <Suspense fallback='Loading...'>
+              <lazyAbout/>
+            </Suspense>
+          }/>
+        <Route path='/order-summary' element={<OrderSummary/>}/>
+        <Route path='/products' element={<ProductsComponent/>}>
+          <Route index element={<FeaturedProducts/>}/>
+          <Route path='featured' element={<FeaturedProducts/>}/>
+          <Route path='new' element={<Newproducts/>}/>
+        </Route>
+        <Route path='/users' element={<UserComponent/>}>
+          <Route path=':userId' element={<UserDetails/>}/>
+          <Route path='admin' element={<Admin/>}/>
+        </Route>
+        <Route path='/profile' element={
+          <RequireAuth>
+            <Profile/>
+          </RequireAuth>
+        }/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='*' element={<NoMatchComponent/>}/>
+      </Routes>
+    </AuthProvider>
   );
 }
 
