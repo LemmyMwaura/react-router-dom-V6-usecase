f6fb9531459c8da88f35e68d087a1c5823f34cc7
Lemmy Mwaura <lemminton@gmail.com>
2022-06-07 22:51:11 +0300

    feat: setup users component

---
+++ src/components/Users.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Outlet, useSearchParams } from 'react-router-dom'
+
+const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const showActiveUsers = searchParams.get('filter') === 'active'
+  return (
+    <div>
+      <Outlet/>
+      <div>
+        <button onClick={() => setSearchParams({filter: 'active'})}>Active Users</button>
+        <button onClick={() => setSearchParams({})}>Reset Filter</button>
+      </div>
+      {showActiveUsers ? (
+        <h2>showing active users</h2>
+      ) : (
+        <h2>Showing all users</h2>
+      )}
+    </div>
+  )
+}
+
+export default Users
